8c64a96fa3a2556d6686c22ad3e285a86f7bd6a8
Duc Nguyen <31971333+minhducubc97@users.noreply.github.com>
2017-11-12 12:09:20 -0800

    Add files via upload

---
+++ Deblur (without kernel)/DBmethod2_Richardson-Lucy.py
@@ -0,0 +1,45 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.ndimage.filters import convolve
+from PIL import Image
+import cv2
+import scipy.misc
+from scipy.misc import imread
+                  
+from scipy.signal import convolve2d as conv2
+
+from skimage import color, restoration
+
+image = imread('dog.jpg')
+astro = color.rgb2gray(image)
+
+psf = np.ones((5, 5)) / 25
+astro = conv2(astro, psf, 'same')
+# Add Noise to Image
+astro_noisy = astro.copy()
+astro_noisy += (np.random.poisson(lam=25, size=astro.shape) - 10) / 255.
+
+# Restore Image using Richardson-Lucy algorithm
+deconvolved_RL = restoration.richardson_lucy(astro_noisy, psf, iterations=50)
+
+scipy.misc.imsave('outfile.jpg', deconvolved_RL)
+
+fig, ax = plt.subplots(nrows=1, ncols=3, figsize=(8, 5))
+plt.gray()
+
+for a in (ax[0], ax[1], ax[2]):
+       a.axis('off')
+
+ax[0].imshow(astro)
+ax[0].set_title('Original Data')
+
+ax[1].imshow(astro_noisy)
+ax[1].set_title('Noisy data')
+
+ax[2].imshow(deconvolved_RL, vmin=astro_noisy.min(), vmax=astro_noisy.max())
+ax[2].set_title('Restoration using\nRichardson-Lucy')
+
+
+fig.subplots_adjust(wspace=0.02, hspace=0.2,
+                    top=0.9, bottom=0.05, left=0, right=1)
+plt.show()
