4fea1d67d11128abd81cf1dfb66da13435b9fd99
Duc Nguyen <31971333+minhducubc97@users.noreply.github.com>
2017-11-12 12:10:57 -0800

    Update and rename DBmethod4_Combine+highpassfilter.py to DBmethod4_Gaussianhighpassfilter.py

--- Deblur (without kernel)/DBmethod4_Gaussianhighpassfilter.py
+++ Deblur (without kernel)/DBmethod4_Gaussianhighpassfilter.py
@@ -8,19 +8,13 @@ import scipy.misc
 im = Image.open('dog.jpg')
 data = np.array(im, dtype=float)
 
-im2 = Image.open('deblurred_dog.jpg')
-data2 = np.array(im2, dtype=float)
-
-im3 = Image.open('sixtrial.jpg')
-data3 = np.array(im, dtype=float)
-
 # Another way of making a highpass filter is to simply subtract a lowpass
 # filtered image from the original. Here, we'll use a simple gaussian filter
 # to "blur" (i.e. a lowpass filter) the original.
-lowpass = ndimage.gaussian_filter(data2, 10)
-gauss_highpass = data2 - lowpass
+lowpass = ndimage.gaussian_filter(data, 10)
+gauss_highpass = data - lowpass
 
-out = data*0.3 + gauss_highpass*0.35 + (data2 - lowpass)*0.2 + data3*0.15
+out = data*0.5 + gauss_highpass*0.5
 
 scipy.misc.imsave('deblur.jpg', out)
 
