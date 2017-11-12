565d5ebd87b527eaca9356f39a59157eda319933
Duc Nguyen <31971333+minhducubc97@users.noreply.github.com>
2017-11-12 12:03:26 -0800

    Delete [BEST] DBmethod5_unsharp.py

--- [BEST] DBmethod5_unsharp.py
+++
@@ -1,6 +0,0 @@
-import cv2
-
-image = cv2.imread("dog.jpg")
-gaussian_3 = cv2.GaussianBlur(image, (9,9), 10.0)
-unsharp_image = cv2.addWeighted(image, 8, gaussian_3, -7, 0, image)
-cv2.imwrite("dog_unsharp.jpg", unsharp_image)
